f9588f88ddfb95d0068615b4f9631e5308284c48
MiohitoKiri5474 <lltzpp@gmail.com>
2024-03-15 15:36:04 +0800

    test: 💍 Add tests about CreateTable, ConnectDB and InsertAd
    Add unit tests about CreateTable, ConnectDB amd InsertAd into CI/CD to
    check there is not any error .

--- db/models_test.go
+++ db/models_test.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"testing"
+	"time"
 )
 
 func TestCreate(t *testing.T) {
@@ -14,4 +15,29 @@ func TestCompressJSON(t *testing.T) {
 	if ExpectRes != CompressJSON(OriList) {
 		t.Error("Value Error")
 	}
+}
+
+func TestCreateTable(t *testing.T) {
+	BuildDB("123.db")
+	CreateTable("123.db")
+}
+
+func TestInsertAd(t *testing.T) {
+	InsertAd(Ad{
+		Title:   "Testing Ad",
+		StartAt: time.Now(),
+		EndAt:   time.Now().AddDate(0, 1, 0),
+		Conditions: Conditions{
+			AgeStart: 18,
+			AgeEnd:   30,
+			Gender:   []string{"M", "F"},
+			Country:  []string{"TW", "JP"},
+			Platform: []string{"Android", "iOS"},
+		},
+	})
+}
+
+func TestConnectDB(t *testing.T) {
+	BuildDB("123.db")
+	ConnectDB("123.db")
 }
