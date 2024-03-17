8ce6f24558933b82e0535a73de97bb7cbf239490
MiohitoKiri5474 <lltzpp@gmail.com>
2024-03-17 15:59:12 +0800

    feat: 🎸 Implement query function
    Implement query function, the api will get the conditions and pass them
    to database to filter the ads, then convert the data returned by
    database into Json to return.
    
    BREAKING CHANGE: 🧨 Implement query function of api

--- cmd/AD_Post/main.go
+++ cmd/AD_Post/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"AD_Post/db"
+	"AD_Post/models"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -13,7 +14,7 @@ import (
 
 func main() {
 	DDBFileName := "ad.db"
-	models.ConnectDB(DDBFileName)
+	db.ConnectDB(DDBFileName)
 
 	router := mux.NewRouter()
 	router.HandleFunc("/api/v1/ad", list_data).Methods("GET")
@@ -21,6 +22,17 @@ func main() {
 
 	fmt.Println("Server is running")
 	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func PrintAd(input models.Ad) {
+	fmt.Println("title:\t\t", input.Title)
+	fmt.Println("StartAt:\t", input.StartAt)
+	fmt.Println("EndAt:\t\t", input.EndAt)
+	fmt.Println("AgeStart:\t", input.AgeStart)
+	fmt.Println("AgeEnd:\t\t", input.AgeEnd)
+	fmt.Println("Gender:\t\t", input.Gender)
+	fmt.Println("Country:\t", input.Country)
+	fmt.Println("Platform:\t", input.Platform)
 }
 
 func list_data(w http.ResponseWriter, r *http.Request) {
@@ -36,26 +48,40 @@ func list_data(w http.ResponseWriter, r *http.Request) {
 	country := QueryParams.Get("country")
 	platform := QueryParams.Get("platform")
 
-	res, err := models.QueryAd(offset, limit, age, gender, country, platform)
+	res, err := db.QueryAd(offset, limit, age, gender, country, platform)
 	if err != nil {
 		http.Error(w, "Failed to query data from database", http.StatusInternalServerError)
 		return
 	}
+
+	var ResSlice []models.ResJson
 	for _, i := range res {
-		fmt.Println(i.Title)
+		ResSlice = append(ResSlice, models.ResJson{Title: i.Title, EndAt: i.EndAt})
 	}
+	OriJson := struct {
+		Item []models.ResJson `json:"item"`
+	}{Item: ResSlice}
+	ResJson, err := json.Marshal(OriJson)
+	if err != nil {
+		http.Error(w, "Error marshaling JSON:", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(ResJson)
 }
 
 func add_data(w http.ResponseWriter, r *http.Request) {
 	// add new data
 	var userData models.JsonParse
 	err := json.NewDecoder(r.Body).Decode(&userData)
+	fmt.Println(r.Body)
 	if err != nil {
 		http.Error(w, "Failed to parse JSON", http.StatusBadRequest)
 		return
 	}
 
-	err = models.InsertAd(userData)
+	err = db.InsertAd(userData)
 	if err != nil {
 		http.Error(w, "Failed to add data to database", http.StatusInternalServerError)
 		return
