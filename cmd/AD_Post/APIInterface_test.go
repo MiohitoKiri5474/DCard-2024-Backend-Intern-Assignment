9aa09ef1263365d513c33519c6802b87188934a0
MiohitoKiri5474 <lltzpp@gmail.com>
2024-03-24 14:36:38 +0800

    test: 💍 Implement unit test about API interface
    Implement unit tests about API interface, also implement benckmark
    testings.

--- cmd/AD_Post/APIInterface_test.go
+++ cmd/AD_Post/APIInterface_test.go
@@ -1,69 +1,112 @@
 package main
 
 import (
+	"AD_Post/db"
+	"AD_Post/models"
 	"bytes"
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
 	"net/http/httptest"
-	"reflect"
-	"strings"
 	"testing"
 	"time"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
-func TestAddData(t *testing.T) {
-	mx := http.NewServeMux()
-	mx.HandleFunc("/ad", AddData)
-
-	Json := `{"title": "Dcard Intern", "startAt": "2024-1-31T03:00:00.000Z", "endAt": "2024-4-6T03:00:00.000Z", "conditions": {"ageStart": 18, "ageEnd": 24, "gender": ["M", "F"], "country": ["TW", "JP", "US"], "platform": ["ios", "android", "web"]}}`
-	reader := strings.NewReader(Json)
-
-	req, err := http.NewRequest(http.MethodPost, "/ad", reader)
+func BuildDB() {
+	// Create DB
+	sqldb, err := gorm.Open(sqlite.Open("ad.db"), &gorm.Config{})
 	if err != nil {
-		t.Error(err.Error())
+		log.Fatal("Failed to connect to database:", err)
 	}
+	sqldb.AutoMigrate(&models.Ad{})
+	db.ConnectDB("ad.db")
+}
 
+func CreateJson() []byte {
+	// Input data declare
+	Json := map[string]interface{}{
+		"title":   "Dcard Intern",
+		"startAt": time.Now(),
+		"endAt":   time.Now().AddDate(0, 1, 0),
+		"conditions": map[string]interface{}{
+			"ageStart": 18,
+			"ageEnd":   24,
+			"gender":   []string{"M", "F"},
+			"platform": []string{"iOS", "Android", "web"},
+			"country":  []string{"TW", "JP", "US"},
+		},
+	}
+	JsonBody, _ := json.Marshal(Json)
+	return JsonBody
+}
+
+func TestAddData(t *testing.T) {
+	BuildDB()
+	JsonBody := CreateJson()
+
+	// Create request/recorder and set header
+	req, err := http.NewRequest("POST", "/ad", bytes.NewBuffer(JsonBody))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
 	w := httptest.NewRecorder()
 
-	mx.ServeHTTP(w, req)
+	// Add data into database
+	AddData(w, req)
 
-	resp := w.Result()
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf(w.Body.String())
-		t.Errorf("Response code is %v", resp.StatusCode)
+	// Check result
+	if w.Code != http.StatusCreated {
+		t.Errorf("expected status %d; got %d", http.StatusOK, w.Code)
+		t.Errorf("%s", w.Body)
 	}
 }
 
 func BenchmarkAddData(b *testing.B) {
-	conditions, _ := json.Marshal(map[string]interface{}{
-		"AgeStart": 18,
-		"AgeEnd":   24,
-		"Gender":   []string{"M", "F"},
-		"Country":  []string{"TW", "JP", "US"},
-		"Platform": []string{"ios", "android", "web"},
-	})
-	Json, _ := json.Marshal(map[string]interface{}{
-		"Title":      "Dcard Intern",
-		"StartAt":    time.Now(),
-		"EndAt":      time.Now().AddDate(0, 1, 0),
-		"Conditions": conditions,
-	})
-
+	BuildDB()
+	JsonBody := CreateJson()
 	for i := 0; i < b.N; i++ {
-		http.NewRequest("POST", "/ad", bytes.NewBuffer(Json))
+		// Create request/recorder and set header
+		req, _ := http.NewRequest("POST", "/ad", bytes.NewBuffer(JsonBody))
+		req.Header.Set("Content-Type", "application/json")
+		w := httptest.NewRecorder()
+
+		// Add data into database
+		AddData(w, req)
 	}
 }
 
 func TestListData(t *testing.T) {
-	_, err := http.NewRequest("GET", "/ad?offset=0&limit=5", nil)
+	BuildDB()
+
+	// Create request and recoder
+	req, err := http.NewRequest("GET", "/ad?offset=0&limit=5", nil)
 	if err != nil {
 		t.Error(err.Error())
+	}
+	w := httptest.NewRecorder()
+
+	// List data from database with conditions
+	ListData(w, req)
+
+	// Check result
+	if w.Code != http.StatusOK {
+		t.Errorf("expected status %d; got %d", http.StatusOK, w.Code)
+		t.Errorf("%s", w.Body)
 	}
 }
 
 func BenchmarkListData(b *testing.B) {
+	BuildDB()
 	for i := 0; i < b.N; i++ {
-		http.NewRequest("GET", "/ad", nil)
+		// Create request and recoder
+		req, _ := http.NewRequest("GET", "/ad?offset=0&limit=5", nil)
+		w := httptest.NewRecorder()
+
+		// List data from database with conditions
+		ListData(w, req)
 	}
 }
